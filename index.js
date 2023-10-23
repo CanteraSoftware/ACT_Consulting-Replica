5056e6052160eb7d1674eb8e86a10b4df8ff43d0
Fabrizio Gonzalez <fabrizioleandrogonzalez03@gmail.com>
2023-10-23 11:34:22 -0300

    navBar terminado

---
+++ index.js
@@ -0,0 +1,13 @@
+const button = document.querySelectorAll('.buttons-navbar li a');
+const navbar = document.querySelector('.bg-nav');
+const contactNav = document.querySelector('#navBar--contacts')
+document.addEventListener('scroll', () =>{
+    if (window.scrollY > 200) {
+        navbar.style.top = 0    
+        navbar.classList.add('scrolled');
+        button.forEach(item => item.classList.add('scrolled'))
+    }else{
+        navbar.classList.remove('scrolled')
+        button.forEach(item => item.classList.remove('scrolled'))
+    }
+})
